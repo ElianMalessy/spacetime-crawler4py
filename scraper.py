43651999b411aa7f49bea423cd9f33c64391c74e
ElianMalessy <eql458@gmail.com>
2024-10-28 20:14:25 -0700

    fixed informational token handling

--- scraper.py
+++ scraper.py
@@ -6,47 +6,22 @@ from collections import defaultdict
 
 class Scraper:
     # Subdomains of uci.edu to crawl within the styx web cache.
-    _allowed_domains = {
-        "ics.uci.edu",
-        "cs.uci.edu",
-        "informatics.uci.edu",
-        "stat.uci.edu",
-    }
+    _allowed_domains = frozenset([ "ics.uci.edu",
+                                  "cs.uci.edu",
+                                  "informatics.uci.edu",
+                                  "stat.uci.edu" ])
 
     # URL Query parameters that definitely indicate crawler traps.
-    _trap_params = {
-        "reply", "comment", "message", "print", "format", "output",
-        "preview", "draft", "share", "invite", "action", "do"
-    }
+    _trap_params = frozenset( [ "reply", "comment", "message", "print", "format", "output",
+                               "preview", "draft", "share", "invite", "action", "do" ] )
 
     # URL Query parameters that might unintentionally cause crawler traps.
     # Still visit these pages, just strip these parameters from URLs.
-    _ordering_params = {
-        "sort", "order", "filter"
-    }
+    _ordering_params = frozenset( [ "sort", "order", "filter" ] )
 
     # Set of English words to ignore. Pulled from the resource linked in the
     # Canvas assignment documentation: https://www.ranks.nl/stopwords
-    _stopwords = {
-        "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at",
-        "be", "because", "been", "before", "being", "below", "between", "both", "but", "by",
-        "can't", "cannot", "could", "couldn't",
-        "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during",
-        "each",
-        "few", "for", "from", "further",
-        "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's",
-        "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself",
-        "let's",
-        "me", "more", "most", "mustn't", "my", "myself",
-        "no", "nor", "not",
-        "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own",
-        "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such",
-        "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too",
-        "under", "until", "up",
-        "very",
-        "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't",
-        "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"
-    }
+    _stopwords = frozenset( [ "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves" ] )
 
 
     def __init__(self):
@@ -128,19 +103,26 @@ class Scraper:
         text.lower()
         tokens = re.findall(r'[^\W_]{2,}', text)
         anchors = soup.find_all('a', href=True)
-        anchors_ratio = len(anchors) / len(tokens)
-        # print(tokens)
-        # print(len(tokens))
+        buttons = soup.find_all('button')
+
+        n_informational_tokens = sum(token not in self._stopwords for token in tokens)
+
+        n_anchor_tokens = sum(token not in self._stopwords for anchor in anchors for token in re.findall(r'[^\W_]{2,}', anchor.get_text()))
+        n_button_tokens = sum(token not in self._stopwords for button in buttons for token in re.findall(r'[^\W_]{2,}', button.get_text()))
+
+        # Remove tokens from the anchor and button text from the total token count.
+        n_informational_tokens -= n_anchor_tokens
+        n_informational_tokens -= n_button_tokens
+                    
+        # print(n_informational_tokens)
 
         MAX_HTML_SIZE = 500000
         MIN_TOKENS = 50
         html_too_large = html_size > MAX_HTML_SIZE
-        not_enough_tokens = len(tokens) < MIN_TOKENS
-        not_enough_tokens_for_large_html = html_size > MAX_HTML_SIZE - 200000 and (len(tokens) < MIN_TOKENS * 2)
-        # less than 100 tokens and more than 10 anchors
-        high_anchors_to_tokens_ratio = len(tokens) < MIN_TOKENS * 2 and anchors_ratio >= 0.1
+        not_enough_tokens = n_informational_tokens < MIN_TOKENS
+        not_enough_tokens_for_large_html = html_size > MAX_HTML_SIZE - 200000 and (n_informational_tokens < MIN_TOKENS * 2)
 
-        if html_too_large or not_enough_tokens or not_enough_tokens_for_large_html or high_anchors_to_tokens_ratio:
+        if html_too_large or not_enough_tokens or not_enough_tokens_for_large_html:
             return []
 
         # Detect and avoid similarity.
@@ -238,7 +220,7 @@ class Scraper:
 
         if any(domain.endswith(d) for d in self._allowed_domains):
             return True
-        # Special case for "today.uci.edu/department/information_computer_sciences/*".
+            # Special case for "today.uci.edu/department/information_computer_sciences/*".
         elif domain == "today.uci.edu" and path.startswith("/department/information_computer_sciences/"):
             return True
 
